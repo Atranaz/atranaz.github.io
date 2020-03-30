5b01915e5a6339446fbd45ee834e0fc1bcc51446
Zaheer Ahmad <ranazee@gmail.com>
2020-03-31 01:30:04 +0400

    Updates

---
+++ precache-manifest.55e2bb0743c0f8b0a5281974761d57fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6223f37a6575df230b968210f5c9ca21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "261abdf28b50fd25810c",
+    "url": "/static/css/main.6577b3b6.chunk.css"
+  },
+  {
+    "revision": "b802bb5f032f42eae9c6",
+    "url": "/static/js/2.84fba8ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.84fba8ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "261abdf28b50fd25810c",
+    "url": "/static/js/main.f649e1e5.chunk.js"
+  },
+  {
+    "revision": "194ec3d5a3dd0691657b",
+    "url": "/static/js/runtime-main.b3bae944.js"
+  },
+  {
+    "revision": "c93b86c33b4f064d34c4f8df6fd24c4b",
+    "url": "/static/media/zee-60.c93b86c3.jpg"
+  }
+]);
